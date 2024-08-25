e226ed40ae3811faef3c39a0343886899e3ad03f
Mattskywalker <mateusejesus@hotmail.com.br>
2024-08-24 22:39:04 -0300

    feat: page breadcrumbs

--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -2,21 +2,24 @@ import { forwardRef, ReactNode } from 'react';
 
 import './styles.css';
 import { Helmet } from 'react-helmet-async';
+import Breadcrumbs, { BreadcrumbsProps } from 'components/Breadcrumbs';
 
 interface Props {
   children: ReactNode;
   meta?: ReactNode;
   title: string;
+  breadCrumbs: BreadcrumbsProps;
 }
 
 const Page = forwardRef<HTMLDivElement, Props>(
-  ({ children, title = '', meta, ...other }, ref) => (
+  ({ children, title = '', meta, breadCrumbs, ...other }, ref) => (
     <>
       <Helmet>
         <title>{`${title} | Getdemo`}</title>
         {meta}
       </Helmet>
 
+      {breadCrumbs && <Breadcrumbs {...breadCrumbs} />}
       <div className="page-content">{children}</div>
     </>
   )
